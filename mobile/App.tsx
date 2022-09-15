fff98d9a06e7f8e411ff1544b25852a570162d61
eduardovsousa <eduardo.varela@outlook.com.br>
2022-09-15 00:00:52 -0300

    Criando a lading page da página mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,46 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StatusBar } from 'react-native';
+import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black } from '@expo-google-fonts/inter';
 
-interface ButtonProps {
-  title: string;
-}
-
-function Button(props: ButtonProps) {
-  return (
-    <TouchableOpacity>
-      <Text style={styles.button}>
-        {props.title}
-      </Text>
-    </TouchableOpacity>
-  )
-}
+import { Home } from './src/screens/Home';
+import { Background } from './src/components/Background'; 
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black
+  });
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello React Native!</Text>
-      <Button title='Send 1' />
-      <Button title='Send 2' />
-      <Button title='Send 3' />
-      <Button title='Hello World!' />
-      <StatusBar style="auto" />
-    </View>
+    <Background>
+      <StatusBar barStyle="light-content"
+      backgroundColor="transparent"
+      translucent
+      />
+      { fontsLoaded ? <Home /> : <Loading /> }
+    </Background>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    color: 'FFF',
-    fontSize: 25,
-  },
-  button: {
-    color: 'FFF',
-    fontSize: 20,
-  }
-});
