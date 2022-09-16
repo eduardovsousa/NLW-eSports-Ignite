df2917e23680b0d36d26fa5f903adf6002a6be40
eduardovsousa <eduardo.varela@outlook.com.br>
2022-09-15 23:02:04 -0300

    Conexão do mobile com backend e criação da página de anúncios

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,9 @@
 import { StatusBar } from 'react-native';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black } from '@expo-google-fonts/inter';
 
-import { Home } from './src/screens/Home';
 import { Background } from './src/components/Background'; 
 import { Loading } from './src/components/Loading';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -16,7 +16,7 @@ export default function App() {
       backgroundColor="transparent"
       translucent
       />
-      { fontsLoaded ? <Home /> : <Loading /> }
+      { fontsLoaded ? <Routes /> : <Loading /> }
     </Background>
   );
 }
