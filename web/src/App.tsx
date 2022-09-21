e22265ae1c95009843b1b525a0c7f4c9dcf98ed4
eduardovsousa <eduardo.varela@outlook.com.br>
2022-09-21 03:43:46 -0300

    Criação do formulário de Login

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -8,6 +8,7 @@ import logoImg from "./assets/logo-nlw-esports.svg";
 import { CreateAdModal } from "./components/CreateAdModal";
 import { GameBanner } from "./components/GameBanner";
 import { CreateAdBanner } from "./components/CreateAdBanner";
+import LoginModal from "./components/LoginModal";
 
 interface Game {
   id: string;
@@ -30,34 +31,39 @@ function App() {
   }, []);
 
   return (
-    <div className="max-w-[1344px] mx-auto my-20 flex flex-col items-center ">
-      <img src={logoImg} alt="logo da nlw esports" />
-
-      <h1 className="text-6xl text-white font-black mt-20">
-        Seu{" "}
-        <span className="text-transparent bg-nlw-gradient bg-clip-text">
-          duo
-        </span>{" "}
-        está aqui.
-      </h1>
-
-      <div className="grid grid-cols-6 gap-6 mt-16">
-        {games.map((game) => {
-          return (
-            <GameBanner
-              key={game.id}
-              title={game.title}
-              bannerUrl={game.bannerUrl}
-              adsCount={game._count.ads}
-            />
-          );
-        })}
-      </div>
-
+    <div>
       <Dialog.Root>
-        <CreateAdBanner />
-        <CreateAdModal />
+        <LoginModal />
       </Dialog.Root>
+
+      <div className="max-w-[1344px] mx-auto my-20 flex flex-col items-center ">
+        <img src={logoImg} alt="logo da nlw esports" />
+
+        <h1 className="text-6xl text-white font-black mt-20">
+          Seu{" "}
+          <span className="text-transparent bg-nlw-gradient bg-clip-text">
+            duo
+          </span>{" "}
+          está aqui.
+        </h1>
+
+        <div className="grid grid-cols-6 gap-6 mt-16">
+          {games.map((game) => {
+            return (
+              <GameBanner
+                key={game.id}
+                title={game.title}
+                bannerUrl={game.bannerUrl}
+                adsCount={game._count.ads}
+              />
+            );
+          })}
+        </div>
+        <Dialog.Root>
+          <CreateAdBanner />
+          <CreateAdModal />
+        </Dialog.Root>
+      </div>
     </div>
   );
 }
---
+++ web/src/components/LoginModal.tsx
@@ -0,0 +1,90 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import * as Checkbox from "@radix-ui/react-checkbox";
+
+import { Input } from "./Form/input";
+import { At, Check, Key, SignIn, X } from "phosphor-react";
+
+export function LoginModal() {
+  return (
+    <div>
+      <Dialog.Trigger className="bg-[#661e6552] flex items-center justify-center py-2 px-12 text-white font-bold rounded-lg ml-auto mr-20 mt-4 hover:bg-[#3e3357]">
+        Login
+      </Dialog.Trigger>
+
+      <Dialog.Portal>
+        <Dialog.Overlay className="bg-black/60 inset-0 fixed">
+          <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-black/25">
+          <Dialog.Close className="ml-96 justify-end items-center py-1 px-1 rounded-md bg-zinc-800 hover:bg-red-400"><X size={20} /></Dialog.Close>
+            <Dialog.Title className="text-3xl font-black">
+              Área de Autenticação
+            </Dialog.Title>
+            <Dialog.Description>
+              Faça o login para uma experiência mais imersiva!
+            </Dialog.Description>
+
+            <form className="mt-8 flex flex-col gap-4">
+              <div className="flex flex-row gap-2 items-center">
+                <At size={20} /><label htmlFor="email">E-mail</label>
+                <Input
+                  id="email"
+                  name="email"
+                  type="text"
+                  placeholder="exemplo@email.com.br"
+                  size={30}
+                />
+              </div>
+
+              <div className="flex flex-row gap-2 items-center">
+                <Key size={20}/><label htmlFor="password">Senha</label>
+                <Input
+                  id="password"
+                  name="password"
+                  type="password"
+                  placeholder="*************"
+                  size={30}
+                />
+              </div>
+
+              <label className="mt-2 flex items-center gap-2 text-sm">
+                <Checkbox.Root className="w-5 h-5 p-0.5 bg-zinc-900">
+                  <Checkbox.Indicator>
+                    <Check className="w-4 h-4 text-emerald-400" />
+                  </Checkbox.Indicator>
+                </Checkbox.Root>
+                Manter-me logado
+              </label>
+
+              <div className="flex justify-between">
+                <label className="mt-4 flex justify-center gap-4">
+                  <button
+                    type="submit"
+                    className="bg-violet-900 px-20 h-8 rounded-md font-semibold flex items-center gap-3 hover:bg-violet-800"
+                  >
+                    Logar
+                    <SignIn size={24} />
+                  </button>
+                </label>
+                <label className="mt-4 flex justify-center gap-4">
+                  <button className="bg-zinc-800 px-4 h-8 rounded-md text-sm hover:bg-zinc-900">
+                    Esqueci a senha
+                  </button>
+                </label>
+              </div>
+              <footer>
+                <hr className="mt-10 self-stretch rounded-lg overflow-hidden h-1 bg-nlw-gradient" />
+                <Dialog.Title className="mt-3">
+                  Ainda não é registrado?
+                </Dialog.Title>
+                <Dialog.Trigger className="bg-emerald-600 px-36 mt-4 h-8 rounded-md font-semibold flex items-center hover:bg-emerald-700">
+                  Crie sua conta
+                </Dialog.Trigger>
+              </footer>
+            </form>
+          </Dialog.Content>
+        </Dialog.Overlay>
+      </Dialog.Portal>
+    </div>
+  );
+}
+
+export default LoginModal;
